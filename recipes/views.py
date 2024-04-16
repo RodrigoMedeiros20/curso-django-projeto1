e9678f27ca5ba5506c1370f420d9570ddaf24dcc
rodrigo <rodrigo.rmedeiros20@gmail.com>
2024-04-16 13:09:29 -0300

    Templates

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Home 1')
+    return render(request, 'recipes/home.html')
 
 def sobre(request):
     return HttpResponse('Sobre')
