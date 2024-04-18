7ac53ab6d074dbc04eee75db51bb7da364f972de
RodrigoMedeiros20 <rodrigo.rmedeiros20@gmail.com>
2024-04-18 08:59:18 -0300

    commits

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Rodrigo Medeiros',
+    })
